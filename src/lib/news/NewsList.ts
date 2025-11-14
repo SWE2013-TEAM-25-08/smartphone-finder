cd3695eb031879af33e18dd9d909572d78da6e71
redfox-ac <redfox@yonsei.ac.kr>
2025-11-14 11:34:11 +0900

    ♻ 스마트폰 및 뉴스 리스트 추가
    스마트폰과 뉴스를 리스트로 가져오는 기능을 미리 만든 객체를 사용할 수 있도록 변경

---
+++ src/lib/news/NewsList.ts
@@ -0,0 +1,11 @@
+import type {News} from "$lib/types/News";
+
+export const newsList: News[] = [
+    {
+        title: "테스트",
+        sub: "뉴스 설명",
+        meta: "작성자 · 2025. 8. 22.",
+        image: null,
+        link: "https://google.com/"
+    }
+]
---
+++ src/lib/phone/PhoneList.ts
@@ -0,0 +1,8 @@
+import {galaxyS25} from "$lib/phone/galaxyS25";
+import {galaxyS25Plus} from "$lib/phone/galaxyS25Plus";
+import {galaxyS25Ultra} from "$lib/phone/galaxyS25Ultra";
+import {iPhone17} from "$lib/phone/iPhone17";
+import {iPhone17Pro} from "$lib/phone/iPhone17Pro";
+import {iPhone17ProMax} from "$lib/phone/iPhone17ProMax";
+
+export const phoneList = [galaxyS25, galaxyS25Plus, galaxyS25Ultra, iPhone17, iPhone17Pro, iPhone17ProMax]
