d1cc1f6425a396038b12832b8ac0e1d98fd6949e
redfox-ac <redfox@yonsei.ac.kr>
2025-11-20 00:49:46 +0900

    ✨ 상세 페이지 동적 라우팅 구현
    전자기기를 더 자세히 알아볼 수 있는 상세 페이지를 동적 라우팅으로 보여질 수 있도록 기능 추가

---
+++ src/routes/info/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type {Smartphone} from "$lib/types/Smartphone";
+
+    let { data } = $props();
+    let phone: Smartphone = $derived(data.phone)
+
+    console.log(phone)
+</script>
+
