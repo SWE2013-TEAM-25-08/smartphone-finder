47ffed8e7293c70e1fd0d105737362517daef905
김설아 <seoula05@naver.com>
2025-11-21 14:36:32 +0900

    아이폰 16 플러스 추가

---
+++ src/lib/phone/iPhone16Plus.ts
@@ -0,0 +1,23 @@
+import {Smartphone} from "$lib/types/Smartphone";
+import {Brand} from "$lib/enum/Brand";
+import {CameraLens} from "$lib/enum/CameraLens";
+
+export let iPhone16e = new Smartphone({
+    name: "아이폰 16 플러스",
+    description: "더 과감해진 색상, 프로 부럽지 않은 성능",
+    image: null,
+    specs: ["Apple A18 칩셋", "8GB RAM"],
+    release_date: new Date("2024-09-20"),
+
+    cpu: {single: 3402, multi: 8418},
+    gpu: 1477,
+    battery_time: 9 * 60 + 30,
+    brand: Brand.Apple,
+    camera: CameraLens.TELEPHOTO,
+    display: 0.65,
+    price: 124.2,
+    ram: 8,
+    size: 6.7,
+    storage: 128,
+    weight: 199
+})
