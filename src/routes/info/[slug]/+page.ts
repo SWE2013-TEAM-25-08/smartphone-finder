3b9e7c9b1e49308649fc0864e92487713006cd8f
redfox-ac <redfox@yonsei.ac.kr>
2025-11-23 20:42:44 +0900

    ♻ 스마트폰 데이터 확인 코드 개선
    동적 라우팅 과정에서 스마트폰 객체를 가져오는 기능 개선

--- src/routes/info/[slug]/+page.ts
+++ src/routes/info/[slug]/+page.ts
@@ -1,25 +1,11 @@
 import {error} from '@sveltejs/kit'
-import {iPhone17} from "$lib/phone/iPhone17";
-import {iPhone17Pro} from "$lib/phone/iPhone17Pro";
-import {iPhone17ProMax} from "$lib/phone/iPhone17ProMax";
-import {galaxyS25} from "$lib/phone/galaxyS25";
-import {galaxyS25Plus} from "$lib/phone/galaxyS25Plus";
-import {galaxyS25Ultra} from "$lib/phone/galaxyS25Ultra";
+import {phoneList} from "$lib/phone/PhoneList";
 import type {Smartphone} from "$lib/types/Smartphone";
 
-
-const  matching = {
-    "iphone-17": iPhone17,
-    "iphone-17-pro": iPhone17Pro,
-    "iphone-17-promax": iPhone17ProMax,
-    "galaxy-s25": galaxyS25,
-    "galaxy-s25-plus": galaxyS25Plus,
-    "galaxy-s25-ultra": galaxyS25Ultra,
-}
-
 export const load = async ({ params : { slug }}) => {
-    if (!(slug in matching)) throw error(404)
+    let find = phoneList.find((phone: Smartphone) => phone.link == slug)
+    if (find === undefined) throw error(404)
     return {
-        phone: matching[slug as keyof typeof matching]
+        phone: find
     }
 }
