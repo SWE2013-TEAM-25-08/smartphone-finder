c0745e6ba95172e127623be7ab04a759d52c9c75
redfox-ac <redfox@yonsei.ac.kr>
2025-11-23 20:31:15 +0900

    ✨ 스마트폰 리스트 정렬 기능 추가
    스마트폰 리스트를 시간 순으로 정렬하도록 해 메인 페이지에서 최신 스마트폰이 보이도록 기능 추가

--- src/lib/phone/PhoneList.ts
+++ src/lib/phone/PhoneList.ts
@@ -4,5 +4,10 @@ import {galaxyS25Ultra} from "$lib/phone/galaxyS25Ultra";
 import {iPhone17} from "$lib/phone/iPhone17";
 import {iPhone17Pro} from "$lib/phone/iPhone17Pro";
 import {iPhone17ProMax} from "$lib/phone/iPhone17ProMax";
+import type {Smartphone} from "$lib/types/Smartphone";
 
 export const phoneList = [galaxyS25, galaxyS25Plus, galaxyS25Ultra, iPhone17, iPhone17Pro, iPhone17ProMax]
+    .sort((a: Smartphone, b: Smartphone) => {
+        let time = b.release_date.getTime() - a.release_date.getTime()
+        return time != 0 ? time : a.price - b.price
+    })
