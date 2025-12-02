8edae874dc26354ab1d431050338966ebd096694
redfox-ac <redfox@yonsei.ac.kr>
2025-12-02 16:41:54 +0900

    🐛 희망 가격보다 높은 스마트폰이 추천되는 오류 해결
    질문 화면에서 입력한 희망 가격보다 10만원을 초과하는 스마트폰을 제외하도록 로직 변경

--- src/routes/let-us-know-you/QuestionPrice.svelte
+++ src/routes/let-us-know-you/QuestionPrice.svelte
@@ -30,7 +30,7 @@
 
 {#snippet content()}
     <div class="select-layout">
-        <RangeSlider id="always" suffix="만원" pips rangeFloat range draggy min={10} max={200} step={10} pipstep={1} rangeGapMin={20} first=label last=label bind:values={range} />
+        <RangeSlider id="always" suffix="만원" pips rangeFloat range draggy min={10} max={300} step={10} pipstep={1} rangeGapMin={20} first=label last=label bind:values={range} />
     </div>
 {/snippet}
 
--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -78,6 +78,10 @@
 
     $: filteredPhones = phones
         .filter(phone => {
+            if (table && phone.price > table!.price.range[1] + 10) {
+                console.log(phone.price + " " + table!.price.range[1] + 100_000)
+                return false
+            }
             const brandMatch = selectedBrands.length === 0 || selectedBrands.some(b => b.filter(phone));
             const yearMatch = selectedYears.length === 0 || selectedYears.some(y => y.filter(phone));
             const featureMatch = selectedFeatures.length === 0 || selectedFeatures.some(f => f.filter(phone));
